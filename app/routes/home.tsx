0562af6482b2dec48ef27b97fcadda7d603b2b89
Najmaldin ishag <najmaldinosman@gmail.com>
2025-07-27 23:16:04 +0300

    initial commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import NavBar from "~/Components/NavBar";
+import {resumes} from "~/Constants";
+import ResumeCard from "~/Components/ResumeCard";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url(/images/bg-main.svg)] bg-cover">
+    <NavBar/>
+    <section>
+    <div className="main-section">
+      <h1>Track Your Application & Resume Rating</h1>
+      <h2>Review Your submission and check AI-powered feedback</h2>
+    </div>
+    </section>
+
+    {resumes.length > 0 && (
+      <div className="resume-section">
+        {resumes.map(resume=>(
+
+        <ResumeCard key={resume.id} resume={resume}/>
+        ))}
+      </div>
+    )}
+
+  </main>
 }
