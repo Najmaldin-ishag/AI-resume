e37d16ff867e5135f0962f5cbc48e76808161965
Najmaldin ishag <najmaldinosman@gmail.com>
2025-07-27 23:31:10 +0300

    update home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -14,21 +14,22 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return <main className="bg-[url(/images/bg-main.svg)] bg-cover">
     <NavBar/>
-    <section>
-    <div className="main-section">
+    <section className="main-section">
+      <div className="page-heading py-16">
       <h1>Track Your Application & Resume Rating</h1>
       <h2>Review Your submission and check AI-powered feedback</h2>
-    </div>
-    </section>
+      </div>
+
 
     {resumes.length > 0 && (
-      <div className="resume-section">
+      <div className="resumes-section">
         {resumes.map(resume=>(
 
         <ResumeCard key={resume.id} resume={resume}/>
         ))}
       </div>
     )}
+    </section>
 
   </main>
 }
